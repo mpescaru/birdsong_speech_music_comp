6078900c5642f51582c242fcc55e1679bc96ed40
Maria Pescaru <maria.pescaru@mail.mcgill.ca>
2025-04-22 14:13:52 -0400

    Update prep_paths.m

--- prep_paths.m
+++ prep_paths.m
@@ -1,5 +1,5 @@
 clear
-path_main_fold = 'C:\Users\Sakata-Woolley\Desktop\birdsong_speech_music_comparison'
+path_main_fold = 'PATH\TO\birdsong_speech_music_comp'
 addpath(fullfile(path_main_fold, 'brainstorm3'))
 addpath(path_main_fold)
 brainstorm_protocol = 'STM\'
